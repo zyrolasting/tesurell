f31e62847fef7b3d846a69b16f70d49261e2c9a9
Sage Gerard <sage@sagegerard.com>
2020-05-05 19:10:13 -0400

    Add at-exp-lib dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "tesurell")
-(define deps '("base" "compatibility-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "compatibility-lib" "at-exp-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "at-exp-lib"))
 (define scribblings '(("scribblings/tesurell.scrbl" ())))
 (define pkg-desc "Convenient means to switch #langs within one file.")
 (define version "1.0")
